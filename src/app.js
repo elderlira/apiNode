9f62c685983e36c63783b5a67687be2cbf5a50e9
elderlira <elderfranca@hotmail.com>
2022-10-06 16:05:02 -0300

    added express.json, id in usuarios and post methods to register user

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
 const usuarios = [
     {
-        "nome": 'elder',
-        "cpf":'000010010101',
+        "id": 1,
+        "nome": "elder",
+        "cpf":"000010010101",
         "email": "email@email.com",
         "telefone":"7112345678",
         "logradouro":"endereco",
@@ -21,6 +24,11 @@ app.get('/', (request, response)=> {
 
 app.get('/usuario', (request, response)=> {
     response.status(200).json(usuarios)
+})
+
+app.post('/usuario', (request, response)=> {
+    usuarios.push(request.body)
+    response.status(201).send('Usuario cadastrado com sucesso')
 })
 
 export default app
