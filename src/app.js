3df1fec3cf593efdd7c4e954b092e727ddb453b6
elderlira <elderfranca@hotmail.com>
2022-10-11 15:41:57 -0300

    db connect msg

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import express from 'express'
 import userRouter from './router/user.route.js'
 import cors from 'cors'
+import db from './config/dbConnect.js'
+
+db.on("error", console.log.bind(console, "Erro na conexao com o banco de dados"))
+db.once("open", ()=> {
+    console.log('Conectado com o banco de dados com sucesso!')
+})
 
 
 const app = express()
