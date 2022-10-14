07b681f4e306a4fd0c39380c47786439d58fa7b8
elderlira <elderfranca@hotmail.com>
2022-10-14 18:34:13 -0300

    changed nome to nameCompleto

--- src/model/user.js
+++ src/model/user.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchemas = new mongoose.Schema({
     id: {type: String},
-    nome: {type: String, required: true},
+    nomeCompleto: {type: String, required: true},
     cpf: {type: String, required: true},
     email: {type: String, required: true},
     telefone: {type: String, required: true},
