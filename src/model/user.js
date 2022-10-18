1bd5c860c203d9263f6de74a35c1adaf02b7d6aa
elderlira <elderfranca@hotmail.com>
2022-10-17 21:35:55 -0300

    added key unique CPF and max length some fields

--- src/model/user.js
+++ src/model/user.js
@@ -2,16 +2,16 @@ import mongoose from "mongoose";
 
 const userSchemas = new mongoose.Schema({
     id: {type: String},
-    nomeCompleto: {type: String, required: true},
-    cpf: {type: String, required: true},
+    nomeCompleto: {type: String, required: true, max: 40},
+    cpf: {type: String, required: true, unique: true, max: 11},
     email: {type: String, required: true},
     telefone: {type: String, required: true},
-    cep: {type: String, required: true},
+    cep: {type: String, required: true, max: 7},
     logradouro: {type: String, required: true},
     complemento: {type: String},
     bairro: {type: String, required: true},
-    uf: {type: String, required: true},
-    nivel: {type: String, default: 0},
+    uf: {type: String, required: true, max: 2},
+    nivel: {type: String, default: 0, max: 1},
     servicos: {type: String} //later replace by-> mongose.Schema.Types.ObjectId, ref: 'service' 
 })
 
