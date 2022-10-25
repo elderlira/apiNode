d976d38d3c3122eedd222eafdbc908f13f9ce64b
elderlira <elderfranca@hotmail.com>
2022-10-25 15:26:48 -0300

    added whatsapp and instagram fields

--- src/model/user.js
+++ src/model/user.js
@@ -1,18 +1,21 @@
 import mongoose from "mongoose";
+import { Schema } from "mongoose"
 
-const userSchemas = new mongoose.Schema({
+const userSchemas = new Schema({
     id: {type: String},
     nomeCompleto: {type: String, required: true, max: 40},
     cpf: {type: String, required: true, unique: true, max: 11},
     email: {type: String, required: true},
     telefone: {type: String, required: true},
+    whatsaspp: {type: String},
+    instagram: {type: String},
     cep: {type: String, required: true, max: 7},
     logradouro: {type: String, required: true},
     complemento: {type: String},
     bairro: {type: String, required: true},
     uf: {type: String, required: true, max: 2},
     nivel: {type: String, default: 0, max: 1},
-    servicos: {type: String} //later replace by-> mongose.Schema.Types.ObjectId, ref: 'service' 
+    servicos: {type: String} //later replace by-> Schema.Types.ObjectId, ref: 'service' 
 })
 
 const Users = mongoose.model('users', userSchemas)
