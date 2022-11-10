85bb627665f7aa28959dd4f05fb8a550bca41a47
elderlira <elderfranca@hotmail.com>
2022-11-10 19:46:46 -0300

    added local db

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 import dbCredentials from "./dbCredentials.js"
 
-const enviromentConnect = `mongodb+srv://doctorFenix:${dbCredentials.password}@cluster0.qtlsnmj.mongodb.net/${dbCredentials.db}`
+// const enviromentConnect = `mongodb+srv://doctorFenix:${dbCredentials.password}@cluster0.qtlsnmj.mongodb.net/${dbCredentials.db}`
+const enviromentConnect = `mongodb://localhost:27017/${dbCredentials.db}`
 
 mongoose.connect(enviromentConnect)
 
