d41eae900f426483cf237d167030fcc0519aa210
elderlira <elderfranca@hotmail.com>
2022-12-12 17:20:17 -0300

    created serviceSchema

--- src/model/service.js
+++ src/model/service.js
@@ -2,23 +2,26 @@ import mongoose from "mongoose";
 import { Schema } from "mongoose";
 
 const serviceSchemas = new Schema({
-    id: {type: String },
-    userId: {type: String },
-    serviceOrder: {type: String},
-    droneMark: {type: String },
-    droneModel: {type: String },
-    droneSerial: {type: String },
-    controllerRadio: {type: String },
-    controllerRadioSerial: {type: String },
-    battery: {type: String },
-    batterySerial: {type: String },
-    acessory: {type: String },
-    droneStatus: {type: String },
-    batteryStatus: {type: String },
-    radioStatus: {type: String },
-    imagemStatus: {type: String },
-    gimbalStatus: {type: String },
-    motoresStatus: {type: String }
+    id: { type: String },
+    userId: { type: String },
+    serviceOrder: { type: String },
+    droneMark: { type: String },
+    droneModel: { type: String },
+    droneSerial: { type: String },
+    controllerRadio: { type: String },
+    controllerRadioSerial: { type: String },
+    battery: { type: String },
+    batterySerial: { type: String },
+    acessory: { type: String },
+    droneStatus: { type: String },
+    batteryStatus: { type: String },
+    radioStatus: { type: String },
+    imagemStatus: { type: String },
+    gimbalStatus: { type: String },
+    motoresStatus: { type: String },
+    initialAnalyze: { type: String }
+    // dataRemocao, dataAtualizacao, dataCriacao
+    //idUserRemoca, idUserAtualicacao, idUserCriacao
 })
 
 const Services = mongoose.model('services', serviceSchemas)
