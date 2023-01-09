cd1e3a0194846d037b61388c14cdd14b8e489136
elderlira <elderfranca@hotmail.com>
2023-01-09 19:34:37 -0300

    added router get by id

--- src/router/budget.route.js
+++ src/router/budget.route.js
@@ -4,5 +4,6 @@ import budgetControler from '../controller/budget.js'
 const router = Router()
 
 router.post('/', budgetControler.create)
+router.get('/:id', budgetControler.getById)
 
 export default router
