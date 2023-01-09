78dcc3c5657c109311731a1ce18043744e92ff84
elderlira <elderfranca@hotmail.com>
2023-01-09 19:34:56 -0300

    added router getById

--- src/router/service.route.js
+++ src/router/service.route.js
@@ -4,6 +4,7 @@ import serviceController from '../controller/service.js'
 const router = Router()
 
 router.get('/', serviceController.getAll)
+router.get('/:id', serviceController.getById)
 router.post('/', serviceController.create)
 
 export default router
