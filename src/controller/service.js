cf4ee15843d2994c26dbc0ce9022ebdcf9479495
elderlira <elderfranca@hotmail.com>
2023-01-19 16:07:05 -0300

    fixed bug to show user and service data. deleted console.log

--- src/controller/service.js
+++ src/controller/service.js
@@ -1,51 +1,50 @@
-import Services from '../model/service.js'
-import Users from "../model/User.js"
-import sendEmail from '../Email/sendEmail.js'
-import { json } from 'express'
+import Users from "../model/User.js";
+import Services from "../model/service.js";
+import sendEmail from "../Email/sendEmail.js";
 
 export default {
-
-    getAll(req, res) {
-        Services.find((err, Serv)=> {
-            if(!err) {
-                // console.log(Serv)
-                res.status(200).json(Serv)
-            } else {
-                // console.log(err)
-                res.status(400).json({message: 'Não foi possível obter a lista de serviço ou não existe'})
-            }
-        })
-    },
-
-    getById(req, res) {
-        const id = req.params.id
-        try {
-            Services.find({'_id': id}, (err, Serv) => {
-                if(!err) {
-                    res.status(200).json(Serv)
-                }
-            })
-        } catch (erro) {
-            throw new Error(erro)
-        }
-    },
-
-    create(req, res) {
-        try {
-            const {email, userId} = req.body
-            Users.findById(userId, (err, user)=> {
-                if(!err) {
-                    Services.create(req.body)
-                    const message = 'seu equipamento a ser analisado foi registrado com sucesso'
-                    sendEmail(email, user.nomeCompleto, message)
-                    res.status(201).json({
-                        message,
-                        userData: user.nomeCompleto
-                    })
-                }
-            })
-        } catch (err) {
-            res.status(400).json({message: 'Erro ao registrar o serviço. Favor contact o administrador'})
-        }
+  async getAll(req, res) {
+    try {
+      res.status(200).json(services)
+      const services = await Services.find().populate('userId')
+    } catch(erro) {
+      res.status(200).json({message: `${erro}`})
+      throw new Error(erro)
     }
-}
+  },
+
+  getById(req, res) {
+    const id = req.params.id;
+    try {
+      Services.find({ '_id': id }, (err, Serv) => {
+        if (!err) {
+          res.status(200).json(Serv);
+        }
+      });
+    } catch (erro) {
+      throw new Error(erro);
+    }
+  },
+
+  create(req, res) {
+    try {
+      const { email, userId } = req.body;
+      Users.findById(userId, (err, user) => {
+        if (!err) {
+          Services.create(req.body);
+          const message =
+            "seu equipamento foi registrado com sucesso e passará por analíse dos nossos técnicos e em ate 96h retornaremos com o diagnóstico";
+          sendEmail(email, user, message);
+          res.status(201).json({
+            message,
+            userData: user.nomeCompleto,
+          });
+        }
+      });
+    } catch (err) {
+      res.status(400).json({
+        message: "Erro ao registrar o serviço. Favor contact o administrador",
+      });
+    }
+  },
+};
